d24e35c3e6584ac779434f1aa1b3785cb79f03db
Hojej <hoojej@gmail.com>
2020-05-22 03:21:11 +0200

    Message is too long - error

--- CPISender/CPISender/CPISender.py
+++ CPISender/CPISender/CPISender.py
@@ -1,20 +1,19 @@
 import time
 import socket
 
-for pings in range(10):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(1.0)
-    addr = ("127.0.0.1", 4000)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(1.0)
+addr = ("127.0.0.1", 4000)
 
-    fileName = ""
+file_name = "/home/ubuntu/workspace/CPISender/CPISender/CPISender/15_09_00.0347.bin"
 
-    try:
-        f=open(file_name,"rb") 
-        data = f.read()
-        data, server = client_socket.recvfrom(1024)
-        client_socket.sendto(data,addr)
-        print("Sent")
-    except socket.timeout:
+try:
+	f=open(file_name,"rb") 
+	data = f.read()
+	#data, server = client_socket.recvfrom(1024)
+	client_socket.sendto(data,addr)
+	print("Sent")
+except socket.timeout:
         print('REQUEST TIMED OUT')
 
 client_socket.close()
